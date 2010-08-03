5df33213028b9d42fde5769ca25a85e824cba45f
Paul Pham <ppham@local-box.org>
2010-08-02 19:26:50 -0700

    Test for arbitrary angles, not just pi/2.

--- test_unitary.py
+++ test_unitary.py
@@ -5,6 +5,8 @@ from skc_utils import *
 from skc_basis import *
 from skc_compose import *
 
+import random
+
 # Pauli matrices are unitary
 assert_matrix_unitary(SX.matrix)
 assert_matrix_unitary(SY.matrix)
@@ -17,23 +19,40 @@ assert_matrix_hermitian(SY.matrix)
 assert_matrix_hermitian(SZ.matrix)
 assert_matrix_hermitian(I2.matrix)
 
+##############################################################################
 # SU(2)
 B2 = get_hermitian_basis(d=2)
 
 # Get a random Hermitian
 (H, components) = get_random_hermitian(B2)
 
-U = exp_hermitian_to_unitary(H, B2)
+U = exp_hermitian_to_unitary(H, math.pi/2, B2)
 
-print "U= " + str(U)
+print "U(pi/2)= " + str(U)
 
 assert_matrix_unitary(U)
 
+angle = random.random() * math.pi
+
+U = exp_hermitian_to_unitary(H, angle, B2)
+
+print "U("+str(angle)+")= " + str(U)
+
+assert_matrix_unitary(U)
+
+##############################################################################
+# SU(4)
 B4 = get_hermitian_basis(d=4)
 (H4, components4) = get_random_hermitian(B4)
 
-U4 = exp_hermitian_to_unitary(H4, B4)
+U4 = exp_hermitian_to_unitary(H4, math.pi/2, B4)
 
-print "U4= " + str(U4)
+print "U4(pi/2)= " + str(U4)
+
+assert_matrix_unitary(U4)
+
+U4 = exp_hermitian_to_unitary(H4, angle, B4)
+
+print "U4("+str(angle)+")= " + str(U4)
 
 assert_matrix_unitary(U4)

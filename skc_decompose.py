9744dbf81cd55d9281f2b7c1ee352b661db717ae
Paul Pham <ppham@local-box.org>
2010-08-06 15:07:12 -0700

    Take majority vote on sign of angle based on sign of components.

--- skc_decompose.py
+++ skc_decompose.py
@@ -19,7 +19,7 @@ def get_basis_components(matrix_H, basis):
 	for key,gate in basis.items_minus_identity():
 		kc_alpha = numpy.trace(matrix_H * gate.matrix)
 		component_dict[key] = kc_alpha.real
-		print str(key) + "=> " + str(kc_alpha)
+		#print str(key) + "=> " + str(kc_alpha)
 		# Components should be real, with negligible imaginary parts
 		assert_approx_equals(kc_alpha.imag, 0)
 	
@@ -32,27 +32,27 @@ def get_basis_components(matrix_H, basis):
 	
 	# Check that we really have the norm of the component vector
 	norm = scipy.linalg.norm(component_dict.values())
-	print "norm= " + str(norm)
+	#print "norm= " + str(norm)
 	
-	sign = None
+	sign_plus = 0
+	sign_minus = 0
 
+	# Go through components, count how many are positive and negative
+	# and scale by norm
 	for key,value in component_dict.items():
 		# If a value is close to zero, don't use it to determine sign
-		if (numpy.abs(value) < TOLERANCE):
-			continue
-		if (sign == None):
+		if (numpy.abs(value) > TOLERANCE):
 			if (value > 0):
-				sign = +1
+				sign_plus += 1
 			elif (value < 0):
-				sign = -1
-		else:
-			if (value > 0):
-				assert(sign != -1)
-				#print "assert sign != -1"
-			elif (value < 0):
-				assert(sign != +1)
-				#print "assert sign != +1"
+				sign_minus += 1
 		component_dict[key] = value / norm
+		
+	# Take a majority vote on the sign of the angle
+	if (sign_plus > sign_minus):
+		sign = +1
+	else:
+		sign = -1
 		
 	# Fix sign of components so that they are all positive
 	for k,v in component_dict.items():
@@ -65,14 +65,14 @@ def get_basis_components(matrix_H, basis):
 	# Verify that we can compose the matrix again from these components
 	M = matrix_from_components(component_dict, basis)
 	dist = fowler_distance(M, matrix_H)
-	if (dist > TOLERANCE3):
-		print "dist= " + str(dist)
-		print "M= " + str(M)
+	#if (dist > TOLERANCE3):
+		#print "dist= " + str(dist)
+		#print "M= " + str(M)
 	#assert_approx_equals(dist, 0)
 	
 	# Fix the scale factor to have the right sign
 	K = sign*norm
-	print "sign= " + str(sign)
+	#print "sign= " + str(sign)
 
 	return (component_dict, K)
 
@@ -80,12 +80,12 @@ def get_basis_components(matrix_H, basis):
 def unitary_to_axis(matrix_U, basis):
 	(matrix_V, matrix_W) = diagonalize(matrix_U, basis)
 	
-	print "V= " + str(matrix_V)
-	print "W= " + str(matrix_W)
+	#print "V= " + str(matrix_V)
+	#print "W= " + str(matrix_W)
 	
 	matrix_ln = get_matrix_logarithm(matrix_W)
 	
-	print "matrix_ln= " + str(matrix_ln)
+	#print "matrix_ln= " + str(matrix_ln)
 	
 	# Reconjugate to transform into iH
 	matrix_iH = matrix_V * matrix_ln * matrix_V.I
@@ -93,9 +93,9 @@ def unitary_to_axis(matrix_U, basis):
 	# Factor out -i (since we used -i in exp_hermitian_to_unitary)
 	matrix_H = (-1.0/1j) * matrix_iH
 	
-	print "matrix_H= " + str(matrix_H)
+	#print "matrix_H= " + str(matrix_H)
 	trace_norm = numpy.trace(matrix_H * matrix_H.H)
-	print "trace_norm(H)= " + str(trace_norm)
+	#print "trace_norm(H)= " + str(trace_norm)
 	
 	# Compare the calculated components with our original
 	(components2, K) = get_basis_components(matrix_H, basis)

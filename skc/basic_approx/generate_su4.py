8ddf0dab946c4726613638e23b49c1013248fc1b
Paul Pham <ppham@local-box.org>
2010-08-17 16:55:18 -0700

    Added CNOT.

--- skc/basic_approx/generate_su4.py
+++ skc/basic_approx/generate_su4.py
@@ -17,15 +17,31 @@ T2 = Operator(name="T2", matrix=T2_matrix)
 
 Tinv1_matrix = numpy.kron(T_inv.matrix, I2.matrix)
 Tinv2_matrix = numpy.kron(I2.matrix, T_inv.matrix)
-Tinv1 = Operator(name="Tinv1", matrix=Tinv1_matrix)
-Tinv2 = Operator(name="Tinv2", matrix=Tinv2_matrix)
+Tinv1 = Operator(name="T1d", matrix=Tinv1_matrix)
+Tinv2 = Operator(name="T2d", matrix=Tinv2_matrix)
 
-iset4 = [H1, H2, T1, T2, Tinv1, Tinv2]
+CNot_matrix = matrixify([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
+CNot = Operator(name="CN", matrix=CNot_matrix)
+CNotd = Operator(name="CNd", matrix=CNot_matrix.H)
+
+iset4 = [H1, H2, T1, T2, Tinv1, Tinv2, CNot, CNotd]
+
+for insn in iset4:
+	print str(insn)
 #iset = [H, T, T_inv]
 
 # Simplifying rules
-double_identity_rule = DoubleIdentityRule('I')
+identity_rule = IdentityRule()
+double_H1_rule = DoubleIdentityRule('H1')
+double_H2_rule = DoubleIdentityRule('H2')
+adjoint_rule = AdjointRule()
 
-simplify_rules = [double_identity_rule]
+simplify_rules = [
+	identity_rule,
+	double_H1_rule,
+	double_H2_rule,
+	adjoint_rule
+	]
+#simplify_rules = []
 
-generate_approxes("basic_approxes_su4.pickle", iset4, 7, simplify_rules)
+generate_approxes("basic_approxes_su4.pickle", iset4, 8, simplify_rules)

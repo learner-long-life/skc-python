995592a333e0734b43990439afda225491c09a68
Paul Pham <ppham@local-box.org>
2010-08-25 15:13:59 -0700

    Factored out most of this into skc.basic_approx.process Use this as an example of how to call kdtree building/searching in main sk.

--- tests/test_kdtree.py
+++ tests/test_kdtree.py
@@ -3,31 +3,28 @@ from skc.basic_approx.file import *
 from skc.compose import *
 from skc.decompose import *
 from skc.basis import *
+from skc.basic_approx.process import *
 from skc.basic_approx.search import *
 
-import math
+import time
 
 # Really, we should save the basis to a file with the instruction set, maybe
 basis = get_hermitian_basis(d=2)
 
-tree = build_kdtree("pickles/su2/gen-g", 16, "-1.pickle", basis)
+base_dir = "pickles/su2"
+file_upper = 16
+
+tree = build_kdtree(base_dir+"/gen-g", file_upper, "-1.pickle")
+
+#process_kdtree(base_dir, file_upper)
+
+#tree = load_kdtree(base_dir, str(file_upper))
 
 # This is the random matrix that we are looking for
 (search_U, components, angle) = get_random_unitary(basis)
-search_op = Operator(name="Search", matrix=search_U)
-angle *= 2.0
-# Re-center angles from 0 to 2pi, instead of -pi to pi
-if (angle < 0):
-	for (k,v) in components.items():
-		components[k] = v * -1
-	angle *= -1
-search_op.dimensions = components_to_kdpoint(components, basis, angle)
-print "search.dimensions= " + str(search_op.dimensions)
+op = search_kdtree(tree, search_U, basis)
 
-nearest = tree.query(search_op, t=2) # find nearest 4 points
-
-for op in nearest:
-	print "op= " + str(op)
-	print "op.dims= " + str(op.dimensions)
-	print "fowler_dist(op,U)= " + str(fowler_distance(op.matrix, search_U))
-	print "trace_dist(op,U)= " + str(trace_distance(op.matrix, search_U))
+print "op= " + str(op)
+#print "op.dims= " + str(op.dimensions)
+print "fowler_dist(op,U)= " + str(fowler_distance(op.matrix, search_U))
+print "trace_dist(op,U)= " + str(trace_distance(op.matrix, search_U))

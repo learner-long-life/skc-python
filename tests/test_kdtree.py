289c9bf0a39d8397b1f188f8f78751fa5681d50c
Paul Pham <ppham@local-box.org>
2010-08-25 13:43:12 -0700

    Factored out to skc.basic_approx.search

--- tests/test_kdtree.py
+++ tests/test_kdtree.py
@@ -3,53 +3,27 @@ from skc.basic_approx.file import *
 from skc.compose import *
 from skc.decompose import *
 from skc.basis import *
+from skc.basic_approx.search import *
 
 import math
 
 # Really, we should save the basis to a file with the instruction set, maybe
 basis = get_hermitian_basis(d=2)
 
-filenames = []
-for i in range(1,15):
-	filenames.append("pickles/su2/gen-g"+str(i)+"-1.pickle")
-
-# This is the data that we load from a file
-sequences = []
-for filename in filenames:
-	new_sequences = read_from_file(filename)
-	sequences.extend(new_sequences)
-
-def components_to_kdpoint(components, basis, angle):
-	point = basis.sort_canonical_order(components)
-	point.append(angle) # add the angle as the last component
-	return point
-
-def unitary_to_kdpoint(matrix_U):
-	(components, K, matrix_H) = unitary_to_axis(matrix_U, basis)
-	return components_to_kdpoint(components, basis, K/2.0)
-
-data = []
-# Process this to produce the format the kdtree expects, namely a list of components in each dimension
-for operator in sequences:
-	#print "op= " + str(operator)
-	#print "matrix= " + str(operator.matrix)
-	#operator.dimensions = unitary_to_kdpoint(operator.matrix)
-	#print "dimensions= " + str(operator.dimensions)
-	# Now dimensions is in R^{d^2}
-	data.append(operator)
-	
+tree = build_kdtree("pickles/su2/gen-g", 16, "-1.pickle", basis)
 
 # This is the random matrix that we are looking for
 (search_U, components, angle) = get_random_unitary(basis)
 search_op = Operator(name="Search", matrix=search_U)
+angle *= 2.0
 # Re-center angles from 0 to 2pi, instead of -pi to pi
 if (angle < 0):
-	angle += 2*math.pi
+	for (k,v) in components.items():
+		components[k] = v * -1
+	angle *= -1
 search_op.dimensions = components_to_kdpoint(components, basis, angle)
 print "search.dimensions= " + str(search_op.dimensions)
 
-# Build it! Kablooey
-tree = KDTree.construct_from_data(data)
 nearest = tree.query(search_op, t=2) # find nearest 4 points
 
 for op in nearest:

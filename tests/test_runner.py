fd4e86f8263a23583c787471f7c84f63e8b31f83
Paul Pham <ppham@local-box.org>
2010-11-18 17:30:01 -0800

    Added test_trig

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -10,6 +10,7 @@ import test_similarity_matrix
 import test_simplify
 import test_operator
 import test_hypersphere
+import test_trig
 
 loader = unittest.TestLoader()
 
@@ -17,6 +18,7 @@ suite = unittest.TestSuite()
 suite.addTest(test_recompose.get_suite())
 suite.addTest(test_find_basis.get_suite())
 suite.addTest(test_utils.get_suite())
+suite.addTest(test_trig.get_suite())
 suite.addTest(test_matrix_ln.get_suite())
 suite.addTest(test_diagonalize.get_suite())
 suite.addTest(test_compose.get_suite())

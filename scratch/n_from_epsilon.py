adb12ab097d2e6cf78ee387499e8ee194a6bbf5c
Paul Pham <ppham@local-box.org>
2011-02-08 23:14:56 -0800

    Added scratch file to test eps_from_n

---
+++ scratch/n_from_epsilon.py
@@ -0,0 +1,15 @@
+from skc.utils import *
+import math
+
+c_approx = 4*math.sqrt(2)
+eps_0 = 1.0/32
+
+print "c_approx= " + str(c_approx)
+print "eps_0= " + str(eps_0)
+
+for i in range(1,5):
+	eps = 1.0 / (2**i)
+	n =n_from_eps(eps, eps_0, c_approx)
+	print "n= " + str(n)
+	print "eps= " + str(eps)
+

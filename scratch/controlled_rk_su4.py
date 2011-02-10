b333f31b431d74cd540400532514b6a90a178e61
Paul Pham <ppham@local-box.org>
2011-02-09 16:09:17 -0800

    Time the compilation and also print the length of ancestors.

--- scratch/controlled_rk_su4.py
+++ scratch/controlled_rk_su4.py
@@ -4,16 +4,17 @@ from skc.group_factor import *
 from skc.compose import *
 from skc.basis import *
 import math
+import time
 
 H4 = get_hermitian_basis(d=4)
 theta = math.pi / 4 # 45 degrees
 
 axis = pick_random_axis(H4)
 # Compose a unitary to compile
-matrix_U = matrixify([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,numpy.exp(1j * math.pi / 4)]])
+matrix_U = matrixify([[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,numpy.exp(1j * math.pi / 128)]])
 op_U = Operator(name="U", matrix=matrix_U)
 
-n = 4
+n = 4 
 print "U= " + str(matrix_U)
 print "n= " + str(n)
 
@@ -24,10 +25,17 @@ sk_set_basis(H4)
 #sk_set_axis(X_AXIS)
 sk_build_tree("su4", 6)
 
+begin_time = time.time()
+
 Un = solovay_kitaev(op_U, n)
+
+compile_time = time.time() - begin_time
+
+print "Compile Time: " + str(compile_time)
 print "Approximated U: " + str(Un)
 
 print "Un= " + str(Un.matrix)
+print "len(Un)= " + str(len(Un.ancestors))
 
 print "trace_dist(U,Un)= " + str(trace_distance(Un.matrix, op_U.matrix))
 print "fowler_dist(U,Un)= " + str(fowler_distance(Un.matrix, op_U.matrix))
